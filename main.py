783ba7dc1d606b7febd2fb17e5724bb0e9173af5
leggen-assis <souzaferreira437@gmail.com>
2025-10-13 15:57:58 -0300

    tests: predict file debug

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import asyncio
-from src import data_processing
-from src import train
-from database import get_async_session
-from src.models.casos_covid import CasoCovid
-from sqlalchemy import select
-
-async def consulta_teste():
-    print("\nIniciando consulta de teste assíncrona...")
-    async with get_async_session() as session:
-        query = select(CasoCovid).limit(5) 
-        result = await session.execute(query)
-        casos = result.scalars().all()
-
-        print(f"Encontrados {len(casos)} casos na consulta de teste:")
-        for caso in casos:
-            print(f"  - Data: {caso.datetime}, Estado: {caso.state}, Cidade: {caso.city or 'N/A'}, Novos Casos: {caso.new_confirmed}, Novas Mortes: {caso.new_deaths}, Total de Casos: {caso.last_available_confirmed}")
-    print("Consulta de teste finalizada.")
+from src import predict
+import os
+import mlflow
 
 async def main():
-    data_processing.main_etl_pipeline()
-    
-    train.train_model()
+    mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI", "http://localhost:5001"))
+
+    predict.predict_next_day(run_id = predict.BEST_RUN_ID)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-
