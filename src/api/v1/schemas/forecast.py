a4a4a1e0af5d9acddde99df89a226a2631c582cf
leggen-assis <souzaferreira437@gmail.com>
2025-10-22 14:48:38 -0300

    feat: ForecastResponseByCity schema

--- src/api/v1/schemas/forecast.py
+++ src/api/v1/schemas/forecast.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 from datetime import date
 
 class PredictionRequest(BaseModel):
@@ -8,6 +8,11 @@ class PredictionRequest(BaseModel):
 class ForecastItem(BaseModel):
     date: date
     predicted_value: float
+
+class ForecastResponseByCity(BaseModel):
+    state: str
+    model_run_id: str
+    forecasts: Dict[str, List[ForecastItem]]  
 
 class ForecastResponse(BaseModel):
     state: str
